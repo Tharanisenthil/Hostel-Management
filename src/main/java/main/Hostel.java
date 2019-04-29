777d08aeb81e2b3d2e76c9caed784658310a0f45
tharani as <tharani as@LAPTOP-CFMUGL08>
2019-04-30 00:57:09 +0530

    Done Hostel

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -41,17 +41,26 @@ public class Hostel{
 		               String checkName = splited[0];
 		               //write your code here !!!
 //		               compare check name with name and return true if present and false if not
+	                   if(splited[0].equals(name))
+	                   {
+	                	   return true;
+	                   }
 	               }
-	               
-	               
+	                       
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
-    	   
+    	   try {
+    		   writedata();
+    	   }catch(IOException e)
+    	   {
+    		   System.out.println(e); 
+    	   }
+    	  
        }
 
        public static boolean verifyStudent(int regNo){
@@ -76,7 +85,8 @@ public class Hostel{
     	   boolean chk = true;
     	   
     	   //write your code here
-    	   
+    	   chk = readData(name);
+    	       	   
     	   return chk;
         }
         
